d36a61319d5457e13c6a0706a3deb07713e6f811
BoltovIgnat <p.pivovarov@notamedia.ru>
2021-06-14 15:52:32 +0300

    fix

--- script.ts
+++ script.ts
@@ -1,18 +1,18 @@
 global.fetch = require("node-fetch");
+var fs = require('fs');
 
 function getPosts (url, after, namefile) {
   return new Promise(function (resolve, reject) {
     fetch('https://www.reddit.com/r/' + url + '.json?limit=100&after='+ after).then(function(response) {
       return response.json();
     }).then(function(json) {
-      var fs = require('fs');
       
       fs.appendFile(namefile, JSON.stringify(json.data.children) , (err) => { 
+        resolve(namefile)
         if (err) { 
           console.log(err); 
         } 
       }); 
-
     });
   });
 }
@@ -20,11 +20,28 @@ function getPosts (url, after, namefile) {
 let receipts = new Array();
 
 for (var i = 0; i < 10; i++) {
-  receipts.push(getPosts("facepalm", i*100, "input.json"));
+  receipts.push(getPosts("facepalm", i*100, "input"+ i+".json"));
 }
 
+
 function good() {
-  console.log('ibc good!');
+  
+  console.log('ibc here!');
+  var files = fs.readdirSync(process.cwd());
+  fs.unlinkSync("result.json");
+  for (let i in files){
+      
+    if(files[i].indexOf('input') == 0){
+      
+      let data = fs.readFileSync(files[i], "utf8");
+      fs.appendFile('result.json', data, (err) => { 
+        fs.unlinkSync(files[i]);
+        if (err) { 
+          console.log(err); 
+        }
+      });
+    }
+  }
 }
 
 function wrong(err) {
