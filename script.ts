d4553b0815fcdeade53d2011e1150a838bb4b175
BoltovIgnat <p.pivovarov@notamedia.ru>
2021-06-14 11:51:13 +0300

    fix

--- script.ts
+++ script.ts
@@ -1,19 +1,32 @@
+global.fetch = require("node-fetch");
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const xhr = new XMLHttpRequest();
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === 4) {
-    if(xhr.status === 200){
-      console.log(xhr.responseText)
+function getPosts (url, after, namefile) {
+  return new Promise(function (resolve, reject) {
+    fetch('https://www.reddit.com/r/' + url + '.json?limit=100&after='+ after).then(function(response) {
+      return response.json();
+    }).then(function(json) {
       var fs = require('fs');
-      fs.writeFile ("input.json", JSON.stringify(xhr.responseText), function(err) {
-        if (err) throw err;
-        console.log('complete');
+      fs.writeFile (namefile,  JSON.stringify(json.data.children), function(err) {
+        if (err) throw reject(err);
+          resolve('complete');
         }
     );
-      
-    }
-  }
+    });
+  });
 }
-xhr.open('GET', 'https://www.reddit.com/r/Battlefield.json')
-xhr.send()
+
+let receipts = new Array();
+
+for (var i = 0; i < 10; i++) {
+  receipts.push(getPosts("facepalm", i*100, 'input'+i+".json"));
+}
+
+function good() {
+  console.log('ibc good!');
+}
+
+function wrong(err) {
+  console.log(err);
+}
+
+Promise.all(receipts).then(good).catch(wrong);
